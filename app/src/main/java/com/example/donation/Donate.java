b80ee10297ead095115f6269cca9c05ea1a3f259
Niall Coady <niall_coady7@hotmail.com>
2017-09-15 11:13:35 +0100

    Now able to donate by a text view

--- app/src/main/java/com/example/donation/Donate.java
+++ app/src/main/java/com/example/donation/Donate.java
@@ -5,17 +5,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 
 public class Donate extends AppCompatActivity {
 
+    private int totalDonated = 0;
+    private int target = 10000;
+
     private Button donateButton;
     private RadioGroup paymentMethod;
     private ProgressBar progressBar;
     private NumberPicker amountPicker;
-    private int totalDonated;
+    private EditText amountText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +30,25 @@ public class Donate extends AppCompatActivity {
         paymentMethod = (RadioGroup)findViewById(R.id.paymentMethod);
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
         amountPicker = (NumberPicker)findViewById(R.id.amountPicker);
+        amountText = (EditText)findViewById(R.id.amountText);
 
         amountPicker.setMinValue(0);
         amountPicker.setMaxValue(1000);
-        progressBar.setMax(10000);
+        progressBar.setMax(target);
     }
 
     public void donateButtonPressed(View view) {
-        totalDonated = totalDonated + amountPicker.getValue();
         String method = paymentMethod.getCheckedRadioButtonId() == R.id.payPal ? "PayPal" : "Direct";
+
+        int donatedAmount = amountPicker.getValue();
+        if (donatedAmount == 0) {
+            String text = amountText.getText().toString();
+            if (!text.equals("")) {
+                donatedAmount = Integer.parseInt(text);
+            }
+        }
+        totalDonated = totalDonated + donatedAmount;
         progressBar.setProgress(totalDonated);
-        Log.v("Donate", amountPicker.getValue() + ", method: " + method + "\nCurrent total " + totalDonated);
+        Log.v("Donate", donatedAmount + " donated by " + method + "\nCurrent total " + totalDonated);
     }
 }
